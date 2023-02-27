b459ec6565bc78a303d71262b2c17e317a483b07
AndreySh <shingaryovan@gmail.com>
2023-02-27 23:29:02 +1200

    Program.cs work started

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// выборка из массивов строк элементов длинной 3 и меньше
+string[] array_of_string_1=["Hello", "2", "world", ":-)"];
+string[] array_of_string_2=["1234", "1567", "-2","computer sience"];
+string[] array_of_string_3=["Russia", "Denmark", "Kazan_bral"];
